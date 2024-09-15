eeecdc3d7fe4dc40403e92085f1d697b8a1eb6e7
Youssef Badway <youssefbdaway@gmail.com>
2024-09-15 17:19:31 +0300

    Initial commit

---
+++ app/src/main/java/com/example/masteringmulti_modular/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.masteringmulti_modular
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+/**
+ * Advanced Android Course:MVVM
+ * Multi-Flavored Builds
+ * Data Store
+ * Advanced Gradle
+ * Custom Plugins
+ * Networking
+ */
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
